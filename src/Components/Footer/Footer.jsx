a870e8bf7b84a67a6c64d2d0e121cd876d6c81b7
Pawan Sahani <mpgamer247@gmail.com>
2025-03-20 08:47:32 +0545

    Initial commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Footer.css';
+import wave from '../../img/wave.png';
+import Insta from '@iconscout/react-unicons/icons/uil-instagram';
+import Facebook from '@iconscout/react-unicons/icons/uil-facebook';
+import Github from '@iconscout/react-unicons/icons/uil-github';
+
+function Footer() {
+  return (
+    <div className="footer">
+      <img src={wave} alt="" style={{ width: '100%' }} />
+      <div className="f-content">
+        <span>Study At:Shree Nepal National Secondary School Manawa (Baderwa) Parsa-9</span>
+        <div className="f-icons">
+          <a href="https://www.instagram.com/developer_pawan093/?hl=en">
+            <Insta color="white" size="3rem" />
+          </a>
+          <a href="https://github.com/pawansahani09">
+            <Github color="white" size="3rem" />
+          </a>
+          <a href="https://www.facebook.com/developerpawan093">
+            <Facebook color="white" size="3rem" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/Components/Testimonials/Testimonials.jsx
@@ -0,0 +1,143 @@
+import React from 'react';
+import './Testimonials.css';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import profilePic1 from '../../img/profile1.jpg';
+import profilePic2 from '../../img/profile2.jpg';
+import profilePic3 from '../../img/profile3.jpg';
+import profilePic4 from '../../img/profile4.jpg';
+import profilePic5 from '../../img/profile5.jpeg';
+import profilePic8 from '../../img/profile8.jpg';
+import profilePic9 from '../../img/profile9.jpg';
+import profilePic10 from '../../img/profile10.jpg';
+import profilePic11 from '../../img/profile11.jpg';
+import profilePic12 from '../../img/profile12.jpg';
+import profilePic13 from '../../img/profile13.jpeg';
+import profilePic14 from '../../img/profile14.jpg';
+import profilePic15 from '../../img/profile15.jpeg';
+import { Pagination } from 'swiper';
+import 'swiper/css/pagination';
+import 'swiper/css';
+import SwiperCore, { Autoplay } from 'swiper';
+function Testimonials() {
+  SwiperCore.use([Autoplay]);
+  const clients = [
+    {
+      img: profilePic11,
+      review:
+        'She Is Kripa!.',
+      name: 'GN. Kripa Tamang! Lo She Is General Student',
+    },
+    {
+      img: profilePic1,
+      review:
+        "He Is Roshan!.",
+      name: 'Er. Roshan Sahani! He Is Engineering Student',
+    },
+    {
+      img: profilePic2,
+      review:
+        'He Is Sandip!.',
+      name: 'Er. Sandip Kushwaha! He Is Engineering Student',
+    },
+    {
+      img: profilePic3,
+      review:
+        'He Is Suraj!.',
+      name: 'Er. Suraj Paswan! He Is Engineering Student',
+    },
+    {
+      img: profilePic4,
+      review:
+        'He Is Kadir.',
+      name: 'Er. Abdul Kadir Miya Thakurai! He Is Engineering Student',
+    },
+    {
+      img: profilePic13,
+      review:
+        'He Is Jayram!.',
+      name: 'Er. Jayram Sahani! He Is Engineering Student',
+    },
+    {
+      img: profilePic5,
+      review:
+        'She Is Aava.',
+      name: 'Er. Aava Tharuni! She Is Engineering Student',
+    },
+    {
+      img: profilePic8,
+      review:
+        'She Is Manisha.',
+      name: 'Er. Manisha Sahani! She Is Engineering Student',
+    },
+    {
+      img: profilePic9,
+      review:
+        'She Is Aayushma.',
+      name: 'Er. Aayushma Sahani! She Is Engineering Student',
+    },
+    {
+      img: profilePic10,
+      review:
+        "He Is Gaurav!.",
+      name: 'Er. Gaurav Gauro! He Is Engineering Student',
+    },
+    {
+      img: profilePic12,
+      review:
+        "He Is Bhola!.",
+      name: 'Er. Bhola Mahara! He Is Engineering Student',
+    },
+    {
+      img: profilePic14,
+      review:
+        "He Is Sahas!.",
+      name: 'EH. Sahas! He Is Athetical Hacker',
+    },
+    {
+      img: profilePic15,
+      review:
+        "He Is Rajan!.",
+      name: 'ER. Rajan Raura! He Is Engineering Student',
+    },
+  ];
+  return (
+    <div className="t-wrapper" id="Testimonials">
+      <div className="t-heading">
+        <span>Clients always get </span>
+        <span>Exceptional Work </span>
+        <span>From me...</span>
+        <div
+          className="blur t-blur1"
+          style={{ background: 'var(--purple)' }}
+        ></div>
+        <div className="blur t-blur2" style={{ background: 'skyblue' }}></div>
+      </div>
+
+      {/* Swiper */}
+      <Swiper
+        modules={[Pagination]}
+        slidesPerView={1}
+        pagination={{ clickable: true }}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+      >
+        {clients.map((client, index) => {
+          return (
+            <SwiperSlide key={index}>
+              <div className="testimonials">
+                <img src={client.img} alt="" />
+                <span>{client.review}</span>
+                <span>{client.name}</span>
+              </div>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
+
+export default Testimonials;
