e86c3f3996e5e6ba22a34c0762ea3581306cbaee
Developer PaWaN <mpgamer247@gmail.com>
2025-08-05 23:28:26 +0545

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { ThemeProvider } from './Context.js';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // 🟢 NEW LINE
-
 ReactDOM.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>,
-  document.getElementById('root')
+<ThemeProvider>
+<App />
+</ThemeProvider>,
+
+document.getElementById('root')
 );
 
-// ✅ Enable PWA support
-serviceWorkerRegistration.register();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
